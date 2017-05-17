99655056dbadeb92a4109874413b1899baeb39f0
bram <bramkaashoek@gmail.com>
2017-05-17 14:21:20 +0200

    added score system

--- guess-the-number-game.rb
+++ guess-the-number-game.rb
@@ -2,13 +2,18 @@ def main
   number = rand(100)
   max_guess = 10
   guesscount = 0
+  score = 100
+  puts "Welcome to the guessing game.\nYour current score is 100 points.\nYou will lose 10 points for every guess."
 
   while  guesscount <= max_guess do
+    score -= 10
+
     puts "Make a guess?"
     guess = gets.chomp.to_i
 
     if guess == number
       puts "YOU WIN!!!\nGAME OVER!!!"
+      puts "You have #{score} points left!"
       exit
     end
 
