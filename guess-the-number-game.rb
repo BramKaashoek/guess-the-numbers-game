36e28aadec10adef2575f0573faa9dc10542d3b5
bram <bramkaashoek@gmail.com>
2017-05-17 14:27:18 +0200

    added cheatmode

--- guess-the-number-game.rb
+++ guess-the-number-game.rb
@@ -3,15 +3,16 @@ def main
   max_guess = 10
   guesscount = 0
   score = 100
+  cheatmode = TRUE
+
   puts "Welcome to the guessing game.\nYour current score is 100 points.\nYou will lose 10 points for every guess."
 
-  while  guesscount <= max_guess do
-    score -= 10
+  while  guesscount < max_guess do
 
     puts "Make a guess?"
     guess = gets.chomp.to_i
 
-    if guess == number
+    if guess == number || cheatmode 
       puts "YOU WIN!!!\nGAME OVER!!!"
       puts "You have #{score} points left!"
       exit
@@ -20,15 +21,17 @@ def main
     puts higher_or_lower(number, guess)
 
     guesscount +=1
+    score -= 10
   end
   puts "Uh Oh. You didnt guess the number. That's tough."
 end
 
+
 def higher_or_lower(number, guess)
   if number > guess
-    answer = "Higher!"
+    return "Higher!"
   else
-    answer = "Lower!"
+    return "Lower!"
   end
 end
 
